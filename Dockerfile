4832f2b69fb276b337ddf4c1ba9aa352843cff9d
yuxizhe <yuxizhe2008@163.com>
2016-09-12 01:23:37 +0800

    rsj 获取 rss  转为json

--- Dockerfile
+++
@@ -1,3 +0,0 @@
-FROM node:4-onbuild
-# replace this with your application's default por
-EXPOSE 80
--- index.js
+++ index.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var app = express();
+var rsj = require('rsj');
+//var Promise =require('es6-promise').Promise
+var  firebase = require ("firebase");
+  //import Vue from 'vue'
+  //import VueFire from "vuefire"
+
+  //Vue.use(VueFire);
+  //firebase = new Firebase();
+//   var config = {
+//     apiKey: "AIzaSyD4az7go2CWyb-Yy_2wHISnfoytLEzUg-4",
+//     authDomain: "yuxizhe2008.firebaseapp.com",
+//     databaseURL: "https://yuxizhe2008.firebaseio.com",
+//     storageBucket: "",
+//   };
+//   firebase.initializeApp(config);
+
+// function firebaseData(id){
+//     return firebase.database().ref('/'+id);
+//   };
+
+rsj.r2j('http://www.mp4ba.com/rss.php',function(json) {
+    //var obj = eval('(' + json + ')');//js原生方法
+    var obj = JSON.parse(json);//json.js包
+    console.log(obj);
+    // firebaseData('rss').set(obj);
+    //res.render('index', { title: 'testbook',layout:'layout',nav:'index',rssData:obj});
+});
+
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // views is directory for all template files
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+// app.set('views', __dirname + '/views');
+// app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
-  response.render('pages/index');
+  //response.render('pages/index');
 });
 
 app.listen(app.get('port'), function() {
