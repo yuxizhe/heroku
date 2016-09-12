87fed3d85f1d233e56aa77c61eee16ae8ca69de4
yuxizhe <yuxizhe2008@163.com>
2016-09-12 19:44:33 +0800

    ss

--- index.js
+++ index.js
@@ -21,52 +21,54 @@ function download(url, callback) {
 var url = 'http://www.mp4ba.com/rss.php';
 
 
-// firebase.initializeApp({
-//   serviceAccount: "./yuxizhe2008-1c6760c143d0.json",
-//   databaseURL: "https://yuxizhe2008.firebaseio.com"
-// });
+firebase.initializeApp({
+  serviceAccount: "./yuxizhe2008-1c6760c143d0.json",
+  databaseURL: "https://yuxizhe2008.firebaseio.com"
+});
 
-// function firebaseData(id){
-//     return firebase.database().ref('/'+id);
-//   };
+function firebaseData(id){
+    return firebase.database().ref('/'+id);
+  };
 
  
 
-// download(url, function(data) {
-//   if (data) {
-//     //console.log(data)
-//       parseString(data, function (err, result) {
-//       //firebaseData('rss').push(result);
-//       console.log(result.rss.channel[0].item[0])
-//       });
-//   }
-//   else
-//    console.log("error");
-// });
+download(url, function(data) {
+  if (data) {
+    //console.log(data)
+      parseString(data, function (err, result) {
+      firebaseData('rss').push(result.rss.channel[0].item[0]);
+      console.log(result.rss.channel[0].item[0])
+      });
+  }
+  else
+   console.log("error");
+});
 
-// function settime(){
-//     var time = new Date();
-//     firebaseData('time').push({a:time.toLocaleString()});
-// }
+function settime(){
+    var time = new Date();
+    firebaseData('time').push({a:time.toLocaleString()});
+}
 
-// setInterval(settime,5000);
+setInterval(settime,5000);
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(request, response) {
-      download(url, function(data) {
-        if (data) {
-          //console.log(data)
-            parseString(data, function (err, result) {
-            //firebaseData('rss').push(result);
-            response.send(result.rss.channel[0].item[0])
-            });
+      // download(url, function(data) {
+      //   if (data) {
+      //     //console.log(data)
+      //       parseString(data, function (err, result) {
+      //       //firebaseData('rss').push(result);
+      //       response.send(result.rss.channel[0].item[0])
+      //       });
 
-        }
-        else
-         console.log("error");
-      });
+      //   }
+      //   else
+      //    console.log("error");
+      // });
       
 }
 );
