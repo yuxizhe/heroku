05e1b222f3db7f9ed1db8dde6929cd2a5996367f
yuxizhe <yuxizhe2008@163.com>
2016-09-13 12:02:00 +0800

    棒

--- index.js
+++ index.js
@@ -38,44 +38,44 @@ console.log("push");
 firebaseData('test').on('value',function(re){
     console.log(re.val())
 });
-// download(url, function(data) {
-//   if (data) {
-//     //console.log(data)
-//       parseString(data, function (err, result) {
-//       //firebaseData('rss').push({a:2});
-//       //console.log(result.rss.channel[0].item[0])
-//       });
-//   }
-//   else
-//    console.log("error");
-// });
+download(url, function(data) {
+  if (data) {
+    //console.log(data)
+      parseString(data, function (err, result) {
+      //firebaseData('rss').push({a:2});
+      console.log(result.rss.channel[0].item[0])
+      });
+  }
+  else
+   console.log("error");
+});
 
 function settime(){
     var time = new Date();
     firebaseData('time').push({a:time.toLocaleString()});
     console.log('1');
 
-    // download(url, function(data) {
-    //   if (data) {
-    //     //console.log(data)
-    //       parseString(data, function (err, result) {
-    //       //firebase("rss").remove();
-    //       if(result){
-    //                    var blog;
-    //           for(blog =0; blog<20;blog++){
-    //           var text = result.rss.channel[0].item[blog];
-    //           firebaseData('rss').push({title:text.title,
-    //                                       description:text.description
-    //                                       //category:text.category
-    //                                       });
-    //             console.log(text.title);
-    //               }
-    //            }
-    //      });
-    //   }
-    //   else
-    //    console.log("error");
-    // });
+    download(url, function(data) {
+      if (data) {
+        //console.log(data)
+          parseString(data, function (err, result) {
+          //firebase("rss").remove();
+          if(result){
+                       var blog;
+              for(blog =0; blog<20;blog++){
+              var text = result.rss.channel[0].item[blog];
+              firebaseData('rss').push({title:text.title,
+                                          description:text.description
+                                          //category:text.category
+                                          });
+                console.log(text.title);
+                  }
+               }
+         });
+      }
+      else
+       console.log("error");
+    });
 }
 
 setInterval(settime,3000);
